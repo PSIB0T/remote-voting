15e60a32f14a81be62e1e27388e29c9fabcf7350
Arvind Narayanan <arvindonarayanan@gmail.com>
2018-09-20 11:46:54 +0530

    Add: Voting app boilerplate code

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,9 +4,10 @@ import '../styles/app.css'
 // Import libraries we need.
 import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
+import $ from "jquery"
 
 // Import our contract artifacts and turn them into usable abstractions.
-// import metaCoinArtifact from '../../build/contracts/MetaCoin.json'
+import votingArtifact from '../../build/contracts/Voting.json'
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
 // const MetaCoin = contract(metaCoinArtifact)
@@ -16,59 +17,82 @@ import { default as contract } from 'truffle-contract'
 // For application bootstrapping, check out window.addEventListener below.
 let accounts
 let account
-
+let Voting
+let candidates = { "Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3" }
 const App = {
-  start: function () {
-    const self = this
+	start: function () {
+		// Bootstrap the MetaCoin abstraction for Use.
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    // MetaCoin.setProvider(web3.currentProvider)
+		Voting = contract(votingArtifact)
+		Voting.setProvider(web3.currentProvider)
+		let candidateNames = Object.keys(candidates)
+		for (var i = 0; i < candidateNames.length; i++) {
+			let name = candidateNames[i]
+			Voting.deployed().then(function (contractInstance) {
+				contractInstance.totalVotesFor.call(name).then(function (v) {
+					$("#" + candidates[name]).html(v.toString())
+				})
+			})
+		}
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
-      }
+		// Get the initial account balance so it can be displayed.
+		web3.eth.getAccounts(function (err, accs) {
+			if (err != null) {
+				alert('There was an error fetching your accounts.')
+				return
+			}
 
-      if (accs.length === 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
-        return
-      }
+			if (accs.length === 0) {
+				alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
+				return
+			}
 
-      accounts = accs
-      account = accounts[0]
+			accounts = accs
+			account = accounts[0]
 
-      console.log(account)
-    })
-  },
+			console.log(account)
+		})
+	},
+	voteForCandidate : function () {
+		$("#msg").html(`Vote has been submitted. The vote count will increment as soon as 
+						the vote is recorded on the blockchain. Please wait.`)
+		let candidateName = $("#candidate").val()
+		Voting.deployed().then(function (contractInstance) {
+			contractInstance.voteForCandidate(candidateName, { gas: 140000, from: web3.eth.accounts[0] }).then(function () {
+				let divId = candidates[candidateName]
+				return contractInstance.totalVotesFor.call(candidateName).then(function (v) {
+					$("#" + divId).html(v.toString())
+					$("#msg").html("")
+				})
+			})
+		})
+	}
 }
 
 window.App = App
 
-window.addEventListener('load', function () {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn(
-      'Using web3 detected from external source.' +
+$('document').ready(() => {
+	if (typeof web3 !== 'undefined') {
+		console.warn(
+			'Using web3 detected from external source.' +
       ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
       ' ensure you\'ve configured that source properly.' +
       ' If using MetaMask, see the following link.' +
       ' Feel free to delete this warning. :)' +
       ' http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider)
-  } else {
-    console.warn(
-      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
+		)
+		// Use Mist/MetaMask's provider
+		window.web3 = new Web3(web3.currentProvider)
+	} else {
+		console.warn(
+			'No web3 detected. Falling back to http://127.0.0.1:9545.' +
       ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
       ' Consider switching to Metamask for development.' +
       ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
-  }
+		)
+		// fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+		window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+	}
 
-  App.start()
+	App.start()
 })
